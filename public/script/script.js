2517d678cd91d0001b14af963194bc17c956b9b5
Ravirkt <r_tjikhoeri@live.com>
2025-02-03 12:20:25 +0100

    added visitekaartje #1

---
+++ public/script/script.js
@@ -0,0 +1,52 @@
+// const expandButton = document.querySelector('header button')
+// expandButton.addEventListener('click', expand)
+
+// function expand () {
+//   document.body.classList.toggle('expand')
+// }
+
+// const test = document.querySelector('img');
+
+// test.addEventListener('click',)
+
+// console.log(test);
+
+
+// document.getElementById('foto').onclick 
+// const testingbutton = document.getElementById('button');
+// const infoblock = document.getElementById('foto');
+
+// function changecolor(event) {
+//    console.log(event);
+//   event.target.style.filter = 'none';
+
+// }
+
+
+
+// infoblock.addEventListener("click", changecolor);
+//   event.target.style.backgroundColor = 'black'
+
+
+
+
+
+  // document.getElementsByClassName('info').style.backgroundColor = 'green';
+
+
+  // testingbutton.textContent = 'hdfsdf'
+  // console.log('jfsjdfs');
+  
+  // const foto = document.getElementById('foto');
+  // const card = document.querySelector('.card-inner');
+
+ 
+
+const button1 = document.getElementById("button1");
+const body = document.body;
+
+button1.addEventListener("click", function() {
+  console.log('er is geklikt')
+body.classList.toggle("dark-mode");
+  
+});
